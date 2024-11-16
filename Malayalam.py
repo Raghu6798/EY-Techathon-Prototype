c4d08512a9567c2d50c76abfcb13ea3e66479f9a
Raghu6798 <144773863+Raghu6798@users.noreply.github.com>
2024-11-16 15:33:40 +0530

    Update Malayalam.py

--- Malayalam.py
+++ Malayalam.py
@@ -9,7 +9,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains import load_summarize_chain
 from langchain.schema import Document
 load_dotenv()
-
+##
 llm = ChatGroq(model="llama3-groq-70b-8192-tool-use-preview", api_key=os.getenv("GROQ_API"))
 duckduckgo_search = DuckDuckGoSearchRun()
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
